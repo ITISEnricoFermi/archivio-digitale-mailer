db77c43ec8982779f8e25e7befb8d219284f6163
Ernesto Montada Romero <e.montada0@gmail.com>
2019-03-10 15:17:43 +0100

    Dotenv per development

--- server/server.js
+++ server/server.js
@@ -1,35 +1,39 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mailer = require("./lib/mailer");
-const { main } = require("archivio-digitale-mail");
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").load()
+}
+
+const express = require("express")
+const bodyParser = require("body-parser")
+const mailer = require("./lib/mailer")
+const { main } = require("archivio-digitale-mail")
 
 // VARS
-const port = process.env.API_PORT || 3000;
+const port = process.env.API_PORT || 3000
 
 // Middleware
-const error = require("./middleware/error");
-const asyncMiddleware = require("./middleware/async");
+const error = require("./middleware/error")
+const asyncMiddleware = require("./middleware/async")
 
 // INIT
-const app = express();
+const app = express()
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
-  req.messages = [];
-  next();
-});
+  req.messages = []
+  next()
+})
 
 app.post(
   "/send",
   asyncMiddleware(async (req, res) => {
-    const { subject, recipients, message } = req.body;
-    const mail = await mailer(recipients, subject, await main(message));
-    res.status(200).json(mail);
+    const { subject, recipients, message } = req.body
+    const mail = await mailer(recipients, subject, await main(message))
+    res.status(200).json(mail)
   })
-);
+)
 
-app.use(error());
+app.use(error())
 
-app.listen(port, () => console.log(`Server started on port ${port}.`));
+app.listen(port, () => console.log(`Server started on port ${port}.`))
