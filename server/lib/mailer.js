26de30319fa7e449cd2c33fe47d6228a2953e3b6
Riccardo Sangiorgio <richard.sangiorgio@gmail.com>
2019-06-23 18:20:07 +0200

    Update mailer.js

--- server/lib/mailer.js
+++ server/lib/mailer.js
@@ -3,18 +3,19 @@ const nodemailer = require('nodemailer')
 const { USER, PASSWORD, SERVER, PORT, SECURE, FROM } = process.env
 
 const transporter = nodemailer.createTransport({
-  pool: true,
-  host: SERVER,
-  port: PORT || 587,
+  // pool: true,
+  // host: SERVER,
+  // port: PORT || 587,
+  service: 'Gmail',
   //secure: Boolean(SECURE) || false,
-  secure: false,
+  // secure: false,
   auth: {
     user: USER,
     pass: PASSWORD
-  },
-  tls: {
-    rejectUnauthorized: false
   }
+  // tls: {
+  //   rejectUnauthorized: false
+  // }
 })
 
 module.exports = (to, subject, message) => transporter.sendMail({
